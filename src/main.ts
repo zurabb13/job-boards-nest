a92fd5f0033d141c7a8c2941cf6bdf49ba84c16e
zurabb13 <zura.baramia98@outlook.com>
2024-02-17 16:39:48 +0400

    1

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // app.use(csurf());
-  app.enableCors();
+  app.enableCors({
+    origin: true,
+    credentials: true,
+    allowedHeaders:
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+    methods: 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+    preflightContinue: false,
+  });
   await app.listen(3000);
 }
 bootstrap();
