5bad212b3486adc09ade922176b1851bc84f6cf1
zurabb13 <zura.baramia98@outlook.com>
2024-02-21 11:27:06 +0400

    csurf

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { json } from 'express';
-
-// import * as csurf from 'csurf';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.use(csurf());
-  app.use(json());
-  app.enableCors({
-    origin: true,
-    credentials: true,
-    allowedHeaders:
-      'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-    methods: 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-    preflightContinue: false,
-  });
+
+  app.use(csurf());
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
